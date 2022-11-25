506ab6c245a539e80d131989d128b7fb02f9bcf1
Mengsroin Heng <Mengsroin Heng>
2022-11-25 21:33:45 +0700

    add tailwind

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import ProvinceList from "~/components/ProvinceList";
 
 export default component$(() => {
-  return <div>{/* <ProvinceList /> */}</div>;
+  return <div></div>;
 });
 
 export const head: DocumentHead = {
